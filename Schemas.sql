5f867bb2e61c80b5a98da89a6781d5c2b1353d4b
Aanal Patil <123722815+aanalpatil24@users.noreply.github.com>
2025-07-20 04:18:28 +0530

    Schemas.sql

--- Schemas.sql
+++ Schemas.sql
@@ -1,20 +1,26 @@
--- SCHEMAS of Netflix
+-- Create and use the database
+CREATE DATABASE IF NOT EXISTS netflix_db;
+USE netflix_db;
 
+-- SCHEMAS of Netflix
+-- Drop table if it exists
 DROP TABLE IF EXISTS netflix;
-CREATE TABLE netflix
-(
-	show_id	VARCHAR(5),
-	type    VARCHAR(10),
-	title	VARCHAR(250),
-	director VARCHAR(550),
-	casts	VARCHAR(1050),
-	country	VARCHAR(550),
-	date_added	VARCHAR(55),
-	release_year	INT,
-	rating	VARCHAR(15),
-	duration	VARCHAR(15),
-	listed_in	VARCHAR(250),
-	description VARCHAR(550)
+
+-- Create table
+CREATE TABLE netflix (
+    show_id        VARCHAR(10),
+    type           VARCHAR(20),
+    title          VARCHAR(255),
+    director       TEXT,
+    casts          TEXT,
+    country        TEXT,
+    date_added     VARCHAR(100),
+    release_year   INT,
+    rating         VARCHAR(20),
+    duration       VARCHAR(20),
+    listed_in      TEXT,
+    description    TEXT
 );
 
+-- Check table
 SELECT * FROM netflix;
