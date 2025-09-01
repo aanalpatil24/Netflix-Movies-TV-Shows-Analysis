f00b4f6df3e785b3f75cea9d397584137ae604dc
Anal Patil <123722815+aanalpatil24@users.noreply.github.com>
2025-09-01 17:44:01 +0530

    Update Schemas.sql

--- Schemas.sql
+++ Schemas.sql
@@ -1,25 +1,27 @@
+-- Netflix Movies & TV Shows Analysis Project
+
 -- Create and use the database
 CREATE DATABASE IF NOT EXISTS netflix_db;
 USE netflix_db;
-
+-- DROP DATABaSE IF EXISTS netflix_db;
 -- SCHEMAS of Netflix
 -- Drop table if it exists
 DROP TABLE IF EXISTS netflix;
 
 -- Create table
 CREATE TABLE netflix (
-    show_id        VARCHAR(10),
-    type           VARCHAR(20),
-    title          VARCHAR(255),
-    director       TEXT,
-    casts          TEXT,
-    country        TEXT,
-    date_added     VARCHAR(100),
-    release_year   INT,
-    rating         VARCHAR(20),
-    duration       VARCHAR(20),
-    listed_in      TEXT,
-    description    TEXT
+    show_id  VARCHAR(10),
+    show_type  VARCHAR(20),
+    title  VARCHAR(255),
+    director TEXT,
+    casts  TEXT,
+    country TEXT,
+    date_added  VARCHAR(100),
+    release_year INT,
+    rating  VARCHAR(20),
+    duration  VARCHAR(20),
+    listed_in TEXT,
+    show_description  TEXT
 );
 
 -- Check table
